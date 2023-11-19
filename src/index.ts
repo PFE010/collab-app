748e69e56ac14edfaa86dcc1df4643949ef6c2e6
vidhikakkar99 <vidhikakkar99@gmail.com>
2023-11-19 12:27:45 -0500

    issues closed comment

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ export = (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
+  app.on("issues.closed", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for closing this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
