0bfcc23ae59f8b347ff80da550e687603853f60e
zipthezap <51761473+zipthezap@users.noreply.github.com>
2023-11-21 13:23:23 -0500

    wip

--- ProbotFunction/index.js
+++ ProbotFunction/index.js
@@ -2,7 +2,7 @@ const {
     createAzureFunction,
     createProbot,
   } = require("@probot/adapter-azure-functions");
-  const app = require("../index");
+  const app = require("../src/index");
   module.exports = createAzureFunction(app, {
     probot: createProbot(),
 });
