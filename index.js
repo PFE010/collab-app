925d1619a4b8aeb207ebd280b739bd3bc70bbe06
zipthezap <51761473+zipthezap@users.noreply.github.com>
2023-12-11 17:35:42 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const setupWebhookEvents = require('./src/index.js');
 const setupApiEndpoints = require('./src/exposeRoutes.js');
+const crypto = require('crypto');
 
 module.exports = async (app, { getRouter }) => {
   setupApiEndpoints(app, getRouter);
